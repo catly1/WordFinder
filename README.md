b04945e617e6f1b9c7e52a80ab200aeb519825b0
Carlos Catly <ccatly@gmail.com>
2020-07-30 22:57:31 -0700

    updated the readme. Will start with macthins that have letters on specific locations

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # WordFinder
+## What is this?
+A basic word finder where when given a list of letters and a number for length, it will spit out matching words from its banks.
--- script.js
+++ script.js
@@ -4,8 +4,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const form = document.getElementById("form");
     form.addEventListener('submit', (e)=>{
         e.preventDefault();
-        const bank = document.getElementById("bank").value;
-        const length = document.getElementById("length").value
+        const bank = document.getElementById("bank").value.split("");
+        const length = document.getElementById("length").value;
         findWords(bank, length)
     })
     // findWords()
@@ -18,7 +18,9 @@ function findWords(bank,length, options) {
         words.forEach(word =>{
 
             if (word.length == length){
-                matches.push(word)
+                if (matchWord(word,bank)){
+                    matches.push(word)
+                }
             }
 
         })
@@ -28,11 +30,33 @@ function findWords(bank,length, options) {
 
 function renderResult(matches){
     const resultsElement = document.getElementById("results")
-    console.log(matches)
     matches.forEach((match, index) =>{
         let matchElement = document.createElement("li")
         matchElement.id = index
         matchElement.innerHTML = match
         resultsElement.appendChild(matchElement)
     })
+}
+
+function matchWord(word, bank){
+    let bankCopy = bank.slice();
+    for (let index = 0; index < word.length; index++) {
+        const letter = word[index];
+        let bankIndex = bankCopy.indexOf(letter)
+        if (bankIndex == -1) {
+            return false
+        } else {
+            bankCopy.splice(bankIndex, 1)
+        }
+    }
+    // word.split("").forEach(letter => {
+    //     let index = bankCopy.indexOf(letter)
+    //     if (index == -1){
+    //         return false
+    //     } else {
+    //         bankCopy.splice(index, 1)
+    //     }
+    // })
+    // console.log(bankCopy)
+    return true
 }
