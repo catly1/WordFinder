2b70fc23e683e2912448afe66615809bb6521f49
Carlos Catly <ccatly@gmail.com>
2020-07-30 19:25:35 -0700

    loading a txt file through fetch

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+
+
 window.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById("root").innerHTML = "boob"
+
+    findWords()
+
+
 })
+
+function findWords(length, options) {
+    fetch('words_alpha.txt').then(res => res.text().then(text => console.log(text)))
+}
